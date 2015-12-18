b962cb5d7c5f7d9807f6e6d58fe8e53073d3f8d9
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-12-18 21:57:28 +0100

    Initial Commit

---
+++ HandySwift/HandySwift.h
@@ -0,0 +1,19 @@
+//
+//  HandySwift.h
+//  HandySwift
+//
+//  Created by Cihat Gündüz on 18.12.15.
+//  Copyright © 2015 Flinesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HandySwift.
+FOUNDATION_EXPORT double HandySwiftVersionNumber;
+
+//! Project version string for HandySwift.
+FOUNDATION_EXPORT const unsigned char HandySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandySwift/PublicHeader.h>
+
+
