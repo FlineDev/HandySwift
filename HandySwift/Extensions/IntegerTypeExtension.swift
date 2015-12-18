48c0db184987dd6f2d0c54cbd5588b64ee517c67
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-12-18 22:26:20 +0100

    Add times implementation with tests + Empty playground for usage examples

---
+++ HandySwift/Extensions/IntegerTypeExtension.swift
@@ -0,0 +1,29 @@
+//
+//  IntegerTypeExtension.swift
+//  HandySwift
+//
+//  Created by Cihat Gündüz on 18.12.15.
+//  Copyright © 2015 Flinesoft. All rights reserved.
+//
+
+import Foundation
+
+public extension IntegerType {
+
+    public func times(closure: () -> ()) {
+        guard self > 0 else { return }
+        
+        for _ in 1...self {
+            closure()
+        }
+    }
+    
+    public func times(closure: (Self) -> ()) {
+        guard self > 0 else { return }
+        
+        for i in 1...self {
+            closure(i)
+        }
+    }
+    
+}
---
+++ UsageExamples.playground/Contents.swift
@@ -0,0 +1,2 @@
+import HandySwift
+
