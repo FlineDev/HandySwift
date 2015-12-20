d6e7e0a82510264c99b0f7f037d404a5a48c832e
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-12-20 02:22:11 +0100

    Rename to IntExtension + Update example code and test code

--- HandySwift/HandySwift.h
+++ HandySwift/HandySwift.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double HandySwiftVersionNumber;
 FOUNDATION_EXPORT const unsigned char HandySwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HandySwift/PublicHeader.h>
-
-
