3f429ae5215e37a660cf13805cbb2149b7012fba
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-06-07 07:34:44 +0200

    [Tests] Add basic test for delay method

---
+++ Tests/GlobalsTests.swift
@@ -0,0 +1,30 @@
+//
+//  GlobalsTests.swift
+//  HandySwift
+//
+//  Created by Cihat Gündüz on 07.06.16.
+//  Copyright © 2016 Flinesoft. All rights reserved.
+//
+
+import XCTest
+
+@testable import HandySwift
+
+class GlobalsTests: XCTestCase {
+    
+    func testDelayed() {
+        
+        let expectation = expectationWithDescription("Wait for delay.")
+        
+        let callDate = NSDate()
+        let delaySeconds = 1.5
+        delay(bySeconds: delaySeconds) {
+            XCTAssertEqualWithAccuracy(callDate.timeIntervalSince1970 + delaySeconds, NSDate().timeIntervalSince1970, accuracy: 0.1)
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(delaySeconds + 1.0, handler: nil)
+
+    }
+    
+}
