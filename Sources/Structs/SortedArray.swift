1eae54c94f4bde79ba09f2bb9233747df380afa2
Cihat Gündüz <CihatGuenduez@posteo.de>
2017-01-29 09:27:24 +0100

    Enhance SortedArray to reflect Array naming + Add insert methods

--- Sources/Structs/SortedArray.swift
+++ Sources/Structs/SortedArray.swift
@@ -10,103 +10,106 @@ import Foundation
 
 /// Data structure to keep a sorted array of elements for fast access.
 public struct SortedArray<Element: Comparable> {
-
     // MARK: - Stored Instance Properties
 
-    fileprivate var internalArray: [Element] = []
+    private var internalArray: [Element]
 
     /// Returns the sorted array of elements.
-    public var array: [Element] {
-        return self.internalArray
+    public var array: [Element] { return self.internalArray }
+
+
+    // MARK: - Computed Instance Properties
+
+    /// Accesses a contiguous subrange of the SortedArray's elements.
+    ///
+    /// - Parameter
+    ///   - bounds: A range of the SortedArray's indices. The bounds of the range must be valid indices.
+    public subscript(bounds: Range<Int>) -> SortedArray {
+        return SortedArray(sequence: array[bounds], preSorted: true)
     }
 
 
     // MARK: - Initializers
 
-    /// Creates a new SortedArray with a given array of elements and sorts the elements.
+    /// Creates a new, empty array.
     ///
-    /// - Complexity: Probably O(n * log(n)) -- complexity of `sort()` on an Array.
+    /// For example:
+    ///
+    ///     var emptyArray = SortedArray<Int>()
+    public init() {
+        internalArray = []
+    }
+
+    /// Creates a new SortedArray with a given sequence of elements and sorts its elements.
+    ///
+    /// - Complexity: The same as `sort()` on an Array –- probably O(n * log(n)).
     ///
     /// - Parameters:
     ///     - array: The array to be initially sorted and saved.
-    public init(array: [Element]) {
-        self.init(array: array, preSorted: false)
+    public init<S: Sequence>(_ sequence: S) where S.Iterator.Element == Element {
+        self.init(sequence: sequence, preSorted: false)
     }
 
-    fileprivate init(array: [Element], preSorted: Bool) {
-        if preSorted {
-            self.internalArray = array
-        } else {
-            self.internalArray = array.sorted()
-        }
+    private init<S: Sequence>(sequence: S, preSorted: Bool) where S.Iterator.Element == Element {
+        internalArray = preSorted ? Array(sequence) : Array(sequence).sorted()
     }
 
 
     // MARK: - Instance Methods
 
-    /// Returns the index of the left most matching element. Matching is done via binary search.
+    /// Returns the first index in which an element of the array satisfies the given predicate.
+    /// Matching is done using binary search to minimize complexity.
     ///
     /// - Complexity: O(log(n))
     ///
     /// - Parameters:
-    ///   - predicate: The boolean predicate to match the elements with.
-    /// - Returns: The index of the left most matching element.
-    public func firstMatchingIndex(_ predicate: (Element) -> Bool) -> Int? {
-
-        // check if all elements match
-        if let firstElement = self.array.first {
-            if predicate(firstElement) {
-                return self.array.startIndex
-            }
-        }
-
-        // check if no element matches
-        if let lastElement = self.array.last {
-            if !predicate(lastElement) {
-                return nil
-            }
-        }
+    ///   - predicate: The predicate to match the elements against.
+    /// - Returns: The index of the first matching element or `nil` if none of them matches.
+    public func index(where predicate: (Element) -> Bool) -> Int? {
+        // cover trivial cases
+        guard !array.isEmpty else { return nil }
+        if let first = array.first, predicate(first) { return array.startIndex }
+        if let last = array.last, !predicate(last) { return nil }
 
         // binary search for first matching element
-        var predicateMatched = false
-
-        var lowerIndex = self.array.startIndex
-        var upperIndex = self.array.endIndex
+        var foundMatch = false
+        var lowerIndex = array.startIndex
+        var upperIndex = array.endIndex
 
         while lowerIndex != upperIndex {
+            let middleIndex = lowerIndex + (upperIndex - lowerIndex) / 2
+            guard predicate(array[middleIndex]) else { lowerIndex = middleIndex + 1; continue }
 
-            let middleIndex = lowerIndex.advanced(by: lowerIndex.distance(to: upperIndex) / 2)
-
-            if predicate(self.array[middleIndex]) {
-                upperIndex = middleIndex
-                predicateMatched = true
-            } else {
-                lowerIndex = middleIndex.advanced(by: 1)
-            }
-
+            upperIndex = middleIndex
+            foundMatch = true
         }
 
-        if !predicateMatched {
-            return nil
-        }
-
+        guard foundMatch else { return nil }
         return lowerIndex
     }
 
-    /// Returns a sub array of a SortedArray to a given index without resorting.
+    /// Returns a sub array of a SortedArray up to a given index (excluding it) without resorting.
     ///
     /// - Complexity: O(1)
     ///
     /// - Parameters:
-    ///   - toIndex: The upper bound index until which to include elements.
-    /// - Returns: A new SortedArray instance including all elements until the specified index.
-    public func subArray(toIndex endIndex: Int) -> SortedArray {
+    ///   - index: The upper bound index until which to include elements.
+    /// - Returns: A new SortedArray instance including all elements until the specified index (exluding it).
+    public func prefix(upTo index: Int) -> SortedArray {
+        let subarray = Array(array[array.indices.prefix(upTo: index)])
+        return SortedArray(sequence: subarray, preSorted: true)
+    }
 
-        let range = self.array.startIndex..<endIndex
-        let subArray = Array(self.array[range])
-
-        return SortedArray(array: subArray, preSorted: true)
-
+    /// Returns a sub array of a SortedArray up to a given index (including it) without resorting.
+    ///
+    /// - Complexity: O(1)
+    ///
+    /// - Parameters:
+    ///   - index: The upper bound index until which to include elements.
+    /// - Returns: A new SortedArray instance including all elements until the specified index (including it).
+    public func prefix(through index: Int) -> SortedArray {
+        let subarray = Array(array[array.indices.prefix(through: index)])
+        return SortedArray(sequence: subarray, preSorted: true)
     }
 
     /// Returns a sub array of a SortedArray starting at a given index without resorting.
@@ -114,15 +117,32 @@ public struct SortedArray<Element: Comparable> {
     /// - Complexity: O(1)
     ///
     /// - Parameters:
-    ///   - toIndex: The lower bound index from which to start including elements.
+    ///   - index: The lower bound index from which to start including elements.
     /// - Returns: A new SortedArray instance including all elements starting at the specified index.
-    public func subArray(fromIndex startIndex: Int) -> SortedArray {
+    public func suffix(from index: Int) -> SortedArray {
+        let subarray = Array(array[array.indices.suffix(from: index)])
+        return SortedArray(sequence: subarray, preSorted: true)
+    }
 
-        let range = self.array.indices.suffix(from: startIndex)
-        let subArray = Array(self.array[range])
+    /// Adds a new item to the sorted array.
+    ///
+    /// - Complexity: O(log(n))
+    ///
+    /// - Parameters:
+    ///   - newElement: The new element to be inserted into the array.
+    public mutating func insert(newElement: Element) {
+        let insertIndex = internalArray.index(where: { $0 >= newElement }) ?? internalArray.endIndex
+        internalArray.insert(newElement, at: insertIndex)
+    }
 
-        return SortedArray(array: subArray, preSorted: true)
-
+    /// Adds the contents of a sequence to the SortedArray.
+    ///
+    /// - Complexity: O(n * log(n))
+    ///
+    /// - Parameters:
+    ///   - sequence
+    public mutating func insert<S: Sequence>(contentsOf sequence: S) where S.Iterator.Element == Element {
+        sequence.forEach { insert(newElement: $0) }
     }
 
     /// Removes an item from the sorted array.
@@ -130,9 +150,8 @@ public struct SortedArray<Element: Comparable> {
     /// - Complexity: O(1)
     ///
     /// - Parameters:
-    ///   - atIndex: The index of the element to remove from the sorted array.
-    public mutating func remove(atIndex index: Int) {
-        self.internalArray.remove(at: index)
+    ///   - index: The index of the element to remove from the sorted array.
+    public mutating func remove(at index: Int) {
+        internalArray.remove(at: index)
     }
-
 }
