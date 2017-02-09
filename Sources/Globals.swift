da287afdc3e8465e7eae519243cb62454e6bc298
Cihat Gündüz <CihatGuenduez@posteo.de>
2017-02-09 14:13:21 +0100

    Make closure in delay method unnamed (for cleaner autocompletion)

--- Sources/Globals.swift
+++ Sources/Globals.swift
@@ -14,7 +14,7 @@ import Foundation
 ///   - delayTime: The duration of the delay. E.g. `.seconds(1)` or `.milliseconds(200)`.
 ///   - qosClass: The global QOS class to be used or `nil` to use the main thread. Defaults to `nil`.
 ///   - closure: The code to run with a delay.
-public func delay(by delayTime: Timespan, qosClass: DispatchQoS.QoSClass? = nil, closure: @escaping () -> Void) {
+public func delay(by delayTime: Timespan, qosClass: DispatchQoS.QoSClass? = nil, _ closure: @escaping () -> Void) {
     let dispatchQueue = qosClass != nil ? DispatchQueue.global(qos: qosClass!) : DispatchQueue.main
     dispatchQueue.asyncAfter(deadline: DispatchTime.now() + delayTime, execute: closure)
 }
