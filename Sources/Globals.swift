27af67e509aa1698d5f3ea9cc02a4a8328fb12b7
Kirill Titov <work@kirilltitov.com>
2017-08-11 15:51:17 +0300

    #11 Done

--- Sources/Globals.swift
+++ Sources/Globals.swift
@@ -18,3 +18,26 @@ public func delay(by delayTime: Timespan, qosClass: DispatchQoS.QoSClass? = nil,
     let dispatchQueue = qosClass != nil ? DispatchQueue.global(qos: qosClass!) : DispatchQueue.main
     dispatchQueue.asyncAfter(deadline: DispatchTime.now() + delayTime, execute: closure)
 }
+
+/// Executes passed closure every Timespan interval.
+///
+/// - Parameters:
+///   - interval: Execution interval duration
+///   - queue: Dispatch queue for execution. Defaults to global
+///   - qos: Quality of service. Defaults to .default
+///   - leeway: Allowed leeway between executions. Defaults to .milliseconds(10)
+///   - closure: Block of code to run
+/// - Returns: Timer object
+public func every(
+    _ interval: Timespan,
+    queue: DispatchQueue = DispatchQueue.global(),
+    qos: DispatchQoS = .default,
+    leeway: DispatchTimeInterval = .milliseconds(10),
+    _ closure: @escaping DispatchSourceProtocol.DispatchSourceHandler
+) -> DispatchSourceTimer {
+    let timer = DispatchSource.makeTimerSource(queue: queue)
+    timer.setEventHandler(qos: qos, handler: closure)
+    timer.scheduleRepeating(deadline: .now(), interval: interval, leeway: leeway)
+    timer.resume()
+    return timer
+}
