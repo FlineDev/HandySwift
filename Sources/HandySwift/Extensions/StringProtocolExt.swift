246400b6ef9768a7bc9f4e58f9102664e46d4aea
Cihat Gündüz <commits@cihatguenduez.de>
2020-11-18 15:15:01 +0100

    Make new StringProtocol extensions accessible

--- Sources/HandySwift/Extensions/StringProtocolExt.swift
+++ Sources/HandySwift/Extensions/StringProtocolExt.swift
@@ -4,11 +4,11 @@ import Foundation
 
 extension StringProtocol {
     /// Returns a variation with the first character uppercased.
-    var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
+    public var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
 
     /// Returns a variation with the first character capitalized.
-    var firstCapitalized: String { prefix(1).capitalized + dropFirst() }
+    public var firstCapitalized: String { prefix(1).capitalized + dropFirst() }
 
     /// Returns a variation with the first character lowercased.
-    var firstLowercased: String { prefix(1).lowercased() + dropFirst() }
+    public var firstLowercased: String { prefix(1).lowercased() + dropFirst() }
 }
