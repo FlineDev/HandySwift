a6a9b97f81551012de96f172cd44aa3489489c05
Cihat Gündüz <commits@cihatguenduez.de>
2021-10-21 08:51:10 +0200

    Add `round` and `rounded` methods to Float type

---
+++ Sources/HandySwift/Extensions/FloatExt.swift
@@ -0,0 +1,21 @@
+// Copyright © 2021 Flinesoft. All rights reserved.
+
+import Foundation
+
+extension Float {
+  /// Rounds the value to an integral value using the specified fraction digits and rounding rule.
+  ///
+  /// - NOTE: Dropping the `rule` parameter will default to “schoolbook rounding”.
+  public mutating func round(fractionDigits: Int, rule: FloatingPointRoundingRule = .toNearestOrAwayFromZero) {
+    let divisor = pow(10.0, Float(fractionDigits))
+    self = (self * divisor).rounded(rule) / divisor
+  }
+
+  /// Returns this value rounded to an integral value using the specified fraction digits and rounding rule.
+  ///
+  /// - NOTE: Dropping the `rule` parameter will default to “schoolbook rounding”.
+  public func rounded(fractionDigits: Int, rule: FloatingPointRoundingRule = .toNearestOrAwayFromZero) -> Float {
+    let divisor = pow(10.0, Float(fractionDigits))
+    return (self * divisor).rounded(rule) / divisor
+  }
+}
