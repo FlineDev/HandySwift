894d902fa24a1603c1b893dbb652e3f820fa4c9c
Cihat Gündüz <commits@cihatguenduez.de>
2023-05-14 23:59:07 +0200

    Add convenience Duration creation methods with larger time frames

---
+++ Sources/HandySwift/Extensions/DurationExt.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+extension Duration {
+  /// Construct a `Duration` given a number of weeks represented as a`BinaryInteger`.
+  /// - Returns: A `Duration` representing a given number of weeks.
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  public static func weeks<T: BinaryInteger>(_ weeks: T) -> Duration {
+    self.days(weeks * 7)
+  }
+
+  /// Construct a `Duration` given a number of days represented as a`BinaryInteger`.
+  /// - Returns: A `Duration` representing a given number of days.
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  public static func days<T: BinaryInteger>(_ days: T) -> Duration {
+    self.hours(days * 24)
+  }
+
+  /// Construct a `Duration` given a number of hours represented as a`BinaryInteger`.
+  /// - Returns: A `Duration` representing a given number of hours.
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  public static func hours<T: BinaryInteger>(_ hours: T) -> Duration {
+    self.minutes(hours * 60)
+  }
+
+  /// Construct a `Duration` given a number of minutes represented as a`BinaryInteger`.
+  /// - Returns: A `Duration` representing a given number of minutes.
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  public static func minutes<T: BinaryInteger>(_ minutes: T) -> Duration {
+    self.seconds(minutes * 60)
+  }
+}
