42479108e2966dc86886983b6d91026fcff6c009
Cihat Gündüz <commits@cihatguenduez.de>
2023-07-31 11:35:17 +0200

    Add autoscaleFormatted() function for two-part abbreviated formatting to Duration

--- Sources/HandySwift/Extensions/DurationExt.swift
+++ Sources/HandySwift/Extensions/DurationExt.swift
@@ -34,4 +34,34 @@ extension Duration {
    public static func minutes<T: BinaryInteger>(_ minutes: T) -> Duration {
       self.seconds(minutes * 60)
    }
+
+   @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+   public func autoscaleFormatted() -> String {
+      var leftoverTimeInterval = self.timeInterval
+      let fullDays = Int(leftoverTimeInterval.days)
+
+      leftoverTimeInterval -= .days(Double(fullDays))
+      let fullHours = Int(leftoverTimeInterval.hours)
+
+      leftoverTimeInterval -= .hours(Double(fullHours))
+      let fullMinutes = Int(leftoverTimeInterval.minutes)
+
+      leftoverTimeInterval -= .minutes(Double(fullMinutes))
+      let fullSeconds = Int(leftoverTimeInterval.seconds)
+
+      if fullDays > 0 {
+         guard fullHours != 0 else { return "\(fullDays)d" }
+         return "\(fullDays)d \(fullHours)h"
+      } else if fullHours > 0 {
+         guard fullMinutes != 0 else { return "\(fullHours)h" }
+         return "\(fullHours)h \(fullMinutes)m"
+      } else if fullMinutes > 0 {
+         guard fullSeconds != 0 else { return "\(fullMinutes)m" }
+         return "\(fullMinutes)m \(fullSeconds)s"
+      } else if fullSeconds > 0 {
+         return "\(fullSeconds)s"
+      } else {
+         return "???"
+      }
+   }
 }
