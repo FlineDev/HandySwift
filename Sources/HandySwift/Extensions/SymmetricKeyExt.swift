8e3e134af23a828640257fbadd7b12ae1fbe9b6e
Cihat Gündüz <commits@cihatguenduez.de>
2023-07-31 16:17:34 +0200

    Add Base64 convenience functions to SymmetricKey

---
+++ Sources/HandySwift/Extensions/SymmetricKeyExt.swift
@@ -0,0 +1,18 @@
+#if canImport(CryptoKit)
+import Foundation
+import CryptoKit
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+extension SymmetricKey {
+   var base64EncodedString: String {
+      withUnsafeBytes { Data($0).base64EncodedString() }
+   }
+
+   init?(
+      base64Encoded: String
+   ) {
+      guard let data = Data(base64Encoded: base64Encoded) else { return nil }
+      self.init(data: data)
+   }
+}
+#endif
