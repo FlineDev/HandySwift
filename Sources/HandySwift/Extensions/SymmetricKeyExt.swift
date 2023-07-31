d4c11c2911da4eb7a844b997d8006d6d6c7c34b4
Cihat Gündüz <commits@cihatguenduez.de>
2023-07-31 16:18:33 +0200

    Make SymmetricKey extension public

--- Sources/HandySwift/Extensions/SymmetricKeyExt.swift
+++ Sources/HandySwift/Extensions/SymmetricKeyExt.swift
@@ -4,11 +4,11 @@ import CryptoKit
 
 @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
 extension SymmetricKey {
-   var base64EncodedString: String {
+   public var base64EncodedString: String {
       withUnsafeBytes { Data($0).base64EncodedString() }
    }
 
-   init?(
+   public init?(
       base64Encoded: String
    ) {
       guard let data = Data(base64Encoded: base64Encoded) else { return nil }
