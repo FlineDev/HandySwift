21766d4264c70703752dca4266a4c0cfbc203f02
Cihat Gündüz <commits@cihatguenduez.de>
2023-08-21 08:44:52 +0200

    Document `AutoConformable` typealias

--- Sources/HandySwift/Protocols/AutoConformable.swift
+++ Sources/HandySwift/Protocols/AutoConformable.swift
@@ -1,3 +1,4 @@
 import Foundation
 
+/// Adds conformance to the automatically inferrable protocols ``Hashable``, ``Codable``, and ``Sendable``.
 public typealias AutoConformable = Hashable & Codable & Sendable
