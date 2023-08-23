504efa467f2d21bb189f8e11c63179fd962a66f4
Cihat Gündüz <commits@cihatguenduez.de>
2023-08-23 15:08:41 +0200

    Rename AutoConformable to AutoConforming

--- Sources/HandySwift/Protocols/AutoConforming.swift
+++ Sources/HandySwift/Protocols/AutoConforming.swift
@@ -1,4 +1,4 @@
 import Foundation
 
 /// Adds conformance to the automatically inferrable protocols ``Hashable``, ``Codable``, and ``Sendable``.
-public typealias AutoConformable = Hashable & Codable & Sendable
+public typealias AutoConforming = Hashable & Codable & Sendable
